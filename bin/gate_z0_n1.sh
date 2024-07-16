be79cbc6347292c3ce7a61d23a57ab0a4a7a7551
dhilipsiva <dhilipsiva@pm.me>
2024-07-16 18:34:03 +0530

    chore: Improve recording generation process.
    - Increase the number of seeds from one to three in `gate_z0_n1.sh`
    - Modify `media_z0_n2.sh` by changing the value of the `--seeds` argument to include multiple IP addresses

--- bin/gate_z0_n1.sh
+++ bin/gate_z0_n1.sh
@@ -5,7 +5,8 @@ cargo run -- \
     --node-id 1 \
     --sdn-port 10001 \
     --sdn-zone 0 \
-    --seeds 0@/ip4/127.0.0.1/udp/10000 \
+    --seeds 0@/ip4/127.0.0.1/udp/10000/ip4/192.168.0.108/udp/10000/ip4/172.17.0.1/udp/10000 \
+    --secret insecure \
     gateway \
         --lat 10 \
         --lon 20 \
--- bin/media_z0_n2.sh
+++ bin/media_z0_n2.sh
@@ -5,7 +5,8 @@ cargo run -- \
     --node-id 2 \
     --sdn-port 10002 \
     --sdn-zone 0 \
-    --seeds 1@/ip4/127.0.0.1/udp/10001 \
+    --seeds 0@/ip4/127.0.0.1/udp/10000/ip4/192.168.0.108/udp/10000/ip4/172.17.0.1/udp/10000 \
+    --secret insecure \
     media \
         --allow-private-ip \
         --enable-token-api
